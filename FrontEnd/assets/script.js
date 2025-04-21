5376f81d54e3598279f1ccb827ded0116450ee15
Elom <believeramedji@gmail.com>
2025-04-21 23:38:14 +0200

    Création dynamique des images avec JS

---
+++ FrontEnd/assets/script.js
@@ -0,0 +1,68 @@
+// Prendre les données des travaux et les afficher dans la galerie
+function listerTravaux(travaux) {
+    // Récupération de la div "gallery" dans le HTML où je mets les images
+    const galerie = document.querySelector(".gallery");  
+    
+    // Vider ce qu’il y a dans la galerie (au cas où il y a déjà des choses)
+    galerie.innerHTML = "";  
+    
+    // Pour chaque travail qu’on a reçu (chaque projet), on va créer une image + un titre
+    for (let i = 0; i < travaux.length; i++) {
+      const travail = travaux[i];  // On prend un travail du tableau
+  
+      // Création d'une balise <figure>
+      const figure = document.createElement("figure");
+  
+      // Création d'une image <img>
+      const image = document.createElement("img");
+      image.src = travail.imageUrl;      // On met le lien de l'image
+      image.alt = travail.title;         // On met un texte alternatif pour l'accessibilité
+  
+      // Création d'une légende <figcaption>
+      const legende = document.createElement("figcaption");
+      legende.innerText = travail.title; // On met le titre du travail
+  
+      // On assemble : image + légende dans figure
+      figure.appendChild(image);
+      figure.appendChild(legende);
+  
+      // On ajoute la figure dans la galerie
+      galerie.appendChild(figure);
+    }
+  }
+  
+
+
+// Récupération des données du back-end avec fetch
+fetch("http://localhost:5678/api/works")
+  .then(function(response) {
+    // On transforme la réponse récupérer en JSON
+    return response.json();
+  })
+  .then(function(data) {
+    // Afficher les données récupérées dans la console
+    console.log(data); //  Voir un tableau des projets dans la console
+
+    // Afficher sur la page
+    listerTravaux(data);
+  })
+  .catch(function(error) {
+    console.log("Erreur lors de la récupération des données :", error);
+  });
+
+
+  
+  // Récupération des catégories dans le back-ends avec fetch
+  fetch("http://localhost:5678/api/categories")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data){
+    // Afficher les catégories récupérées dans la console
+    console.log(data);
+    //Afficher les boutons sur la pages
+    //afficherFiltresBoutons(data);
+  })
+  .catch(function(error){
+    console.log("Erreur lors de la récupération des données :", error);
+  });
